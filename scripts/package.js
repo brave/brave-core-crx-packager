8d28375273e0abdb997650894b21177b4f83b39d
Emerick Rogul <erogul@brave.com>
2018-05-08 14:43:45 -0400

    Run code through linter, add exception handlers, add README.md, minor refactoring

--- scripts/package.js
+++ scripts/package.js
@@ -2,85 +2,95 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const commander = require('commander');
-const fs = require('fs');
-const mkdirp = require('mkdirp');
-const path = require('path');
-const replace = require('replace-in-file');
+const commander = require('commander')
+const fs = require('fs')
+const mkdirp = require('mkdirp')
+const path = require('path')
+const replace = require('replace-in-file')
 
-function stageFiles(commander, outputDir, datFile) {
-  const baseDatFile = path.parse(datFile).base;
+process.on('uncaughtException', (err) => {
+  console.error('Caught exception:', err)
+  process.exit(1)
+})
 
-  const originalManifest = path.join('manifests', commander.type + '-manifest.json');
-  const updatedManifest = path.join(outputDir, 'manifest.json');
+process.on('unhandledRejection', (err) => {
+  console.error('Unhandled rejection:', err)
+  process.exit(1)
+})
+
+function stageFiles (commander, outputDir, datFile) {
+  const baseDatFile = path.parse(datFile).base
+
+  const originalManifest = path.join('manifests', commander.type + '-manifest.json')
+  const updatedManifest = path.join(outputDir, 'manifest.json')
 
   const replaceOptions = {
     files: updatedManifest,
     from: /0\.0\.0/g,
-    to: commander.setVersion,
-  };
+    to: commander.setVersion
+  }
 
-  mkdirp.sync(outputDir);
+  mkdirp.sync(outputDir)
 
-  fs.copyFileSync(originalManifest, updatedManifest);
-  fs.copyFileSync(datFile, path.join(outputDir, baseDatFile));
+  fs.copyFileSync(originalManifest, updatedManifest)
+  fs.copyFileSync(datFile, path.join(outputDir, baseDatFile))
 
-  replace.sync(replaceOptions);
+  replace.sync(replaceOptions)
 }
 
-function generateCRX(commander, outputDir, datFile) {
-  const ChromeExtension = require('crx');
+function generateCRX (commander, outputDir, datFile) {
+  const ChromeExtension = require('crx')
   const crx = new ChromeExtension({
     privateKey: fs.readFileSync(commander.key)
-  });
+  })
 
-  const crxFile = commander.type + '.crx';
+  const crxFile = commander.type + '.crx'
 
   crx.load(path.resolve(outputDir))
     .then(() => crx.loadContents())
     .then((archiveBuffer) => {
       crx.pack(archiveBuffer).then((crxBuffer) => {
-        fs.writeFileSync(crxFile, crxBuffer);
-        console.log('Generated ' + crxFile);
-      });
+        fs.writeFileSync(crxFile, crxBuffer)
+        console.log('Generated ' + crxFile)
+      })
     })
     .catch((err) => {
-      console.error(err.stack);
-    });
+      console.error(err.stack)
+      throw err
+    })
+}
+
+function getDATFileFromComponentType () {
+  switch (commander.type) {
+    case 'ad-block-updater':
+      return 'node_modules/ad-block/out/ABPFilterParserData.dat'
+    case 'tracking-protection-updater':
+      return 'node_modules/tracking-protection/data/TrackingProtection.dat'
+    default:
+      throw new Error('Unrecognized component extension type: ' + commander.type)
+  }
 }
 
 commander
   .option('-k, --key <key>', 'private key file path', 'key.pem')
   .option('-t, --type <type>', 'component extension type', /^(ad-block-updater|https-everywhere-updater|tracking-protection-updater)$/i, 'ad-block-updater')
   .option('-s, --set-version <x.x.x>', 'component extension version number')
-  .parse(process.argv);
+  .parse(process.argv)
 
 if (!fs.existsSync(commander.key)) {
-  throw new Error('Missing private key file: ' + commander.key);
+  throw new Error('Missing private key file: ' + commander.key)
 }
 
 if (!commander.setVersion) {
-  throw new Error('Missing required option: --set-version');
+  throw new Error('Missing required option: --set-version')
 }
 
 if (!commander.setVersion.match(/^(\d+\.\d+\.\d+)$/)) {
-  throw new Error('Missing or invalid option: --set-version');
+  throw new Error('Missing or invalid option: --set-version')
 }
 
-let datFile;
+const outputDir = path.join('build', commander.type)
+const datFile = getDATFileFromComponentType(commander.type)
 
-switch (commander.type) {
-case 'ad-block-updater':
-  datFile = 'node_modules/ad-block/out/ABPFilterParserData.dat';
-  break;
-case 'tracking-protection-updater':
-  datFile = 'node_modules/tracking-protection/data/TrackingProtection.dat';
-  break;
-default:
-  throw new Error('Unrecognized component extension type: ' + commander.type);
-}
-
-const outputDir = path.join('build', commander.type);
-
-stageFiles(commander, outputDir, datFile);
-generateCRX(commander, outputDir, datFile);
+stageFiles(commander, outputDir, datFile)
+generateCRX(commander, outputDir, datFile)
