6af9177f0303b2588fc6f6fe2ee49189068bf824
Pranjal Jumde <pranjal.jumde@gmail.com>
2020-06-30 21:10:23 -0700

    use adblock-rust with correct public key for german, indian and lithuania adblock-lists

--- scripts/generateAdBlockRustDataFiles.js
+++ scripts/generateAdBlockRustDataFiles.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const { Engine, lists } = require('adblock-rs')
+const { Engine, lists } = require('adblock-rust')
 const { generateResourcesFile } = require('../lib/adBlockRustUtils')
 const path = require('path')
 const fs = require('fs')
--- scripts/generateManifestForRustAdblock.js
+++ scripts/generateManifestForRustAdblock.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const { lists } = require('adblock-rs')
+const { lists } = require('adblock-rust')
 const fs = require('fs-extra')
 const mkdirp = require('mkdirp')
 const path = require('path')
