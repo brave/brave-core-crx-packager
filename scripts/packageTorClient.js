1c17585de6250b175bc229bf373139ed0fe155ac
Linh Kikuchi <xuongrongl84@gmail.com>
2021-02-12 07:24:32 +0900

    update demo-tor-binaries location

--- scripts/packageTorClient.js
+++ scripts/packageTorClient.js
@@ -44,7 +44,7 @@ const downloadTorClient = (platform) => {
   mkdirp.sync(torPath)
 
   const torClient = path.join(torPath, torFilename)
-  const cmd = 'aws s3 cp ' + torClient + ' ' + torURL + ' .'
+  const cmd = 'aws s3 cp ' + torURL + ' .'
 
   // Download the client
   execSync(cmd)
