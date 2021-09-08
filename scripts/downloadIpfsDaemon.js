22cf83430778fda6aa0175d1428465b11c433688
sergey <spylogsster@gmail.com>
2021-09-08 12:36:38 +0300

    Revert "Merge pull request #237 from brave/issue-16913"
    This reverts commit 62c487c5ccc5c94cb8defc7b05398cdae8b815c8, reversing
    changes made to 8e37e8ef012b1364e04a02d0fcef9dda38e96ba0.

--- scripts/downloadIpfsDaemon.js
+++ scripts/downloadIpfsDaemon.js
@@ -8,56 +8,7 @@ const fs = require('fs')
 const mkdirp = require('mkdirp')
 const path = require('path')
 const util = require('../lib/util')
-const ipfsVersion = '0.9.1'
-const fsRepoVersion = '1.0.0'
-
-// Downloads the current (platform-specific) fs-repo-migration component
-const downloadFsRepoMigration = (platform) => {
-  if (platform !== 'darwin')
-      return
-  const repoPath = path.join('build', 'ipfs-daemon-updater', 'downloads')
-
-  const ipfsDistPrefix = `https://cloudflare-ipfs.com/ipns/dist.ipfs.io/fs-repo-10-to-11/v${fsRepoVersion}/`
-
-  const zipSuffix = '.tar.gz'
-
-  const repoFilename = `fs-repo-10-to-11_v${fsRepoVersion}_${platform}-amd64`
-  const targetFilename = `fs-repo-10-to-11`
-  const ipfsURL = ipfsDistPrefix + repoFilename + zipSuffix
-
-  let sha512IPFS = ''
-
-  switch (platform) {
-    case 'darwin':
-      sha512IPFS = '9a8b8b18c7757e5f2a0abd62dc33fc14616195b27776a967d4adff361a2becedebf286eee156af05bdbf1f39b0da5cc33264939a695a7866abe50a06b817d5cd'
-      break
-    default:
-      throw new Error('fs-repo migration tool download failed; unrecognized platform: ' + platform)
-  }
-
-  mkdirp.sync(repoPath)
-
-  const repoTool = path.join(repoPath, targetFilename)
-  const decompress = `bsdtar xf - -C ${repoPath}`
-  const move = `mv ${path.join(repoPath, 'fs-repo-10-to-11')} ${path.join(repoPath, 'tmp')}`
-  const copy = `cp ${path.join(repoPath, 'tmp', 'fs-repo-10-to-11')} ${repoTool}`
-  const rm = `rm -r ${path.join(repoPath, 'tmp')}`
-  const cmd = `curl -s ${ipfsURL} | ${decompress} && ${move} && ${copy} && ${rm}`
-  console.log(cmd)
-  // Download and decompress the client
-  execSync(cmd)
-
-  // Verify the checksum
-  if (!verifyChecksum(repoTool, sha512IPFS)) {
-    console.error('FsRepoMigration tool checksum verification failed')
-    process.exit(1)
-  }
-
-  // Make it executable
-  fs.chmodSync(repoTool, 0o755)
-
-  return repoTool
-}
+const ipfsVersion = '0.9.0'
 
 // Downloads the current (platform-specific) Ipfs Daemon from ipfs.io
 const downloadIpfsDaemon = (platform) => {
@@ -119,7 +70,6 @@ const verifyChecksum = (file, hash) => {
 util.installErrorHandlers()
 
 downloadIpfsDaemon('darwin')
-downloadFsRepoMigration('darwin')
 downloadIpfsDaemon('linux')
 downloadIpfsDaemon('win32')
 
