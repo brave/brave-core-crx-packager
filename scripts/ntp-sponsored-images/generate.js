677d54e7fb27ab8b2c82a64f43b18ec1ae4cc142
Pete Miller <miller.pete@gmail.com>
2021-09-14 17:31:58 -0700

    NTP SI components are now region and platform specific

--- scripts/ntp-sponsored-images/generate.js
+++ scripts/ntp-sponsored-images/generate.js
@@ -1,37 +1,42 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// you can obtain one at http://mozilla.org/MPL/2.0/. */
 
 const path = require('path')
 const mkdirp = require('mkdirp')
-const fs = require('fs-extra')
 const commander = require('commander')
-const util = require('../lib/util')
+const util = require('../../lib/util')
 const ntpUtil = require('../../lib/ntpUtil')
 const allDestinations = require('./all-region-platforms')
 
-function getTargetRegionList(targetRegions, excludedTargetRegions) {
+// Downloads all current 'active' campaigns from S3 for each region/platform.
+// Can include or exclude specific regions, for performance optimization.
+
+function getTargetRegionList (targetRegions, excludedTargetRegions) {
   let targetRegionList = []
   if (targetRegions === '')
     targetRegionList = allDestinations
   else
     targetRegionList = targetRegions.split(',')
 
-  if (excludedTargetRegions === '')
-    return targetRegionList
+  if (excludedTargetRegions !== '')
+    targetRegionList = targetRegionList.filter(region => !excludedTargetRegions.includes(region))
 
-  return targetRegionList.filter(region => !excludedTargetRegions.includes(region))
+  // TODO(petemill): throw error if any entry isn't in allDestinations
+
+  return targetRegionList
 }
 
 async function generateNTPSponsoredImages (dataUrl, targetRegions, excludedTargetRegions) {
   const rootResourceDir = path.join(path.resolve(), 'build', 'ntp-sponsored-images', 'resources')
   mkdirp.sync(rootResourceDir)
 
-  for (const region of getTargetRegionList(targetRegions, excludedTargetRegions)) {
-    console.log(`Downloading ${region}...`)
-    const targetResourceDir = path.join(rootResourceDir, region)
+  for (const regionPlatformName of getTargetRegionList(targetRegions, excludedTargetRegions)) {
+    console.log(`Downloading ${regionPlatformName}...`)
+    const targetResourceDir = path.join(rootResourceDir, regionPlatformName)
     mkdirp.sync(targetResourceDir)
-    const sourceJsonFileUrl = `${dataUrl}${region}/photo.json`
+    const regionPlatformPath = regionPlatformName.replace('-', '/')
+    const sourceJsonFileUrl = `${dataUrl}${regionPlatformPath}/photo.json`
     await ntpUtil.prepareAssets(sourceJsonFileUrl, targetResourceDir)
   }
 }
@@ -40,19 +45,19 @@ util.installErrorHandlers()
 
 commander
   .option('-d, --data-url <url>', 'url that refers to data that has ntp sponsored images')
-  .option('-t, --target-regions <regions>', 'Comma separated list of regions that should generate SI component. For example: "AU,US,GB"', '')
+  .option('-t, --target-regions <regions>', 'Comma separated list of regions that should generate SI component. For example: "AU-android,US-desktop,GB-ios"', '')
   .option('-u, --excluded-target-regions <regions>', 'Comma separated list of regions that should not generate SI component. For example: "AU,US,GB"', '')
   .parse(process.argv)
 
 let targetRegions = ""
 if (commander.targetRegions) {
   // Stripping unrelated chars.
-  // Only upper case and commas are allowed.
-  targetRegions = commander.targetRegions.replace(/[^A-Z,]/g, "")
+  // Only upper case, commas and dashes are allowed.
+  targetRegions = commander.targetRegions.replace(/[^A-Z,-]/g, "")
 }
 let excludedTargetRegions = ""
 if (commander.excludedTargetRegions) {
-  excludedTargetRegions = commander.excludedTargetRegions.replace(/[^A-Z,]/g, "")
+  excludedTargetRegions = commander.excludedTargetRegions.replace(/[^A-Z,-]/g, "")
 }
 
 generateNTPSponsoredImages(commander.dataUrl, targetRegions, excludedTargetRegions)
