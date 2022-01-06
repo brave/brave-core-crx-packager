53d930cfb8c86a15bd92f927b8d0980afc7fae45
Pete Miller <miller.pete@gmail.com>
2022-01-06 15:46:22 -0800

    Parse ntp-sponsored-images params correctly

---
+++ scripts/ntp-sponsored-images/params.js
@@ -0,0 +1,66 @@
+// Copyright (c) 2022 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// you can obtain one at http://mozilla.org/MPL/2.0/.
+
+const regionPlatformComponentMetadata = require('./region-platform-component-metadata')
+
+/**
+ *
+ *
+ * @param {string|undefined} incoming
+ * @returns string[]
+ */
+function parseRegionListParam (incoming) {
+  if (!incoming || typeof incoming !== 'string') {
+    return []
+  }
+  const incomingClean = incoming.replace(/[^A-Za-z,-]/g, '')
+  return incomingClean.split(',').map(i => i.trim()).filter(i => !!i)
+}
+
+/**
+ *
+ *
+ * @param {string[]} includes
+ * @param {string[]} excludes
+ *
+ * @typedef {{ locale: string, key: string, id: string }} ComponentMetadata
+ *
+ * @returns {typeof regionPlatformComponentMetadata}
+ */
+function getTargetComponentsFromArrays (includes, excludes) {
+  const targetComponents = { }
+  for (const platformRegion of Object.keys(regionPlatformComponentMetadata)) {
+    // Include the component if it's in includes and not in excludes, or includes is empty
+    if ((!includes.length || includes.includes(platformRegion)) && (
+      !excludes.length || !excludes.includes(platformRegion)
+    )) {
+      targetComponents[platformRegion] = regionPlatformComponentMetadata[platformRegion]
+    }
+  }
+  return targetComponents
+}
+
+/**
+ *
+ *
+ * @param {string|undefined} includesParamValue
+ * @param {string|undefined} excludesParamValue
+ *
+ * @returns {typeof regionPlatformComponentMetadata}
+ */
+function getTargetComponents (includesParamValue, excludesParamValue) {
+  const includes = parseRegionListParam(includesParamValue)
+  const excludes = parseRegionListParam(excludesParamValue)
+
+  const targetComponents = getTargetComponentsFromArrays(includes, excludes)
+
+  console.log('included keys: ', includes.join(', '))
+  console.log('excluded keys: ', excludes.join(', '))
+  console.log('resulting target keys:', Object.keys(targetComponents))
+
+  return targetComponents
+}
+
+module.exports.getTargetComponents = getTargetComponents
---
+++ scripts/ntp-sponsored-images/params.test.js
@@ -0,0 +1,34 @@
+// Copyright (c) 2022 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// you can obtain one at http://mozilla.org/MPL/2.0/.
+
+const tap = require('tap')
+const params = require('./params')
+const allComponents = require('./region-platform-component-metadata')
+
+tap.test('ntp getTargetComponentsFromArrays', (t) => {
+  // blank
+  let actual = params.getTargetComponents(undefined, undefined)
+  t.strictSame(actual, allComponents)
+
+  // single
+  actual = params.getTargetComponents('US-android', '')
+  t.strictSame(actual, { 'US-android': allComponents['US-android'] })
+
+  // multiple, exclude bad
+  actual = params.getTargetComponents('US-android,IT,FR-desktop,asd', '')
+  t.strictSame(actual, { 'US-android': allComponents['US-android'], 'FR-desktop': allComponents['FR-desktop'] })
+
+  // strip bad chars but still include
+  actual = params.getTargetComponents('"US-an;droid",')
+  t.strictSame(actual, { 'US-android': allComponents['US-android'] })
+
+  //exclude
+  actual = params.getTargetComponents('', 'US-android')
+  const expectedExclude = { ...allComponents }
+  delete expectedExclude['US-android']
+  t.strictSame(actual, expectedExclude)
+
+  t.end()
+})
