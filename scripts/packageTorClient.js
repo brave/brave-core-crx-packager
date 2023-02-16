846627569a258e68a739dde884408b77ff866225
Linh Kikuchi <xuongrongl84@gmail.com>
2023-02-16 11:04:37 +0900

    Add tor linux-arm64

--- scripts/packageTorClient.js
+++ scripts/packageTorClient.js
@@ -34,6 +34,9 @@ const downloadTorClient = (platform) => {
     case 'linux':
       sha512Tor = '4d2ef4b4b578d6127bd730d6dafb4c32e235d19bb80ae34bcda806fb4809f3a2e3c8e8ce4af30d94ab0f3e93cdb39799a0f7edfb953523591f3ca9253eec9f7d'
       break
+    case 'linux-arm64':
+      sha512Tor = 'ed1d999d07f971251c191bb3adae6cd119309d78d874d5741a7f889d6e6c1b02f7866b87b9eb3a3d09f4068ebf2e285e47d492d77d62a5435d536d90ff8e65b6'
+      break
     case 'win32':
       sha512Tor = '7d3acfdbdb5f6d4c79c60b2a8450a0515be3f3eb7da34d0e1b5d4ebe434e388537f1888746a3c6807d4c9165757593d71c982c0bc90527483e863b79d1000b16'
       break
@@ -137,6 +140,8 @@ util.createTableIfNotExists(commander.endpoint, commander.region).then(() => {
     'darwin', keyParam, commander.publisherProofKey)
   packageTorClient(commander.binary, commander.endpoint, commander.region,
     'linux', keyParam, commander.publisherProofKey)
+  packageTorClient(commander.binary, commander.endpoint, commander.region,
+    'linux-arm64', keyParam, commander.publisherProofKey)
   packageTorClient(commander.binary, commander.endpoint, commander.region,
     'win32', keyParam, commander.publisherProofKey)
 })
