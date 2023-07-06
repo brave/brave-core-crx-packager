497d464a627e703407bad47d0d6c92c2491026c4
Michael Herrmann <michael@herrmann.io>
2023-07-06 16:11:54 +0200

    Update kubo to 0.21.0

--- scripts/packageIpfsDaemon.js
+++ scripts/packageIpfsDaemon.js
@@ -11,7 +11,7 @@ import mkdirp from 'mkdirp'
 import path from 'path'
 import replace from 'replace-in-file'
 import util from '../lib/util.js'
-const ipfsVersion = '0.20.0'
+const ipfsVersion = '0.21.0'
 
 const getIpfsDaemonPath = (os, arch) => {
   const ipfsPath = path.join('build', 'ipfs-daemon-updater', 'downloads')
