d15af237819916e47da6305cac85539191937546
Anton Lazarev <antonok35@gmail.com>
2023-08-07 23:43:30 -0700

    test that resources are valid JS

--- test/generateResourcesFile.js
+++ test/generateResourcesFile.js
@@ -2,6 +2,7 @@ import assert from 'node:assert/strict'
 import test from 'node:test'
 import tmp from 'tmp'
 import fs from 'fs'
+import { spawnSync } from 'child_process'
 
 import { generateResourcesFile } from '../lib/adBlockRustUtils.js'
 
@@ -12,5 +13,14 @@ test('generateResourcesFile', async (t) => {
     const resources = JSON.parse(filedata)
     assert.ok(Object.prototype.toString.call(resources) === '[object Array]')
     assert.ok(resources.length >= 10)
+    for (const r of resources) {
+      assert.ok(r.kind !== undefined)
+      assert.ok(r.kind.mime !== undefined)
+      if (r.kind.mime === 'application/javascript' || r.kind.mime === 'fn/javascript') {
+        const script = atob(r.content)
+        const subprocess = spawnSync('node', ['--check'], { input: script })
+        assert.ok(subprocess.status === 0, 'Resource ' + r.name + ' is not valid JS:\n' + subprocess.stderr.toString() + '\n' + script)
+      }
+    }
   })
 })
