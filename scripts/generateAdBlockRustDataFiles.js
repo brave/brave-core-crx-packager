9bb15aea09f5dc5c2307ed0dee79259a0e7136dd
Anton Lazarev <antonok35@gmail.com>
2023-09-24 19:48:21 -0700

    fix lint error

--- scripts/generateAdBlockRustDataFiles.js
+++ scripts/generateAdBlockRustDataFiles.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import { Engine, FilterFormat, FilterSet, RuleTypes } from 'adblock-rs'
+import { Engine, FilterSet, RuleTypes } from 'adblock-rs'
 import { generateResourcesFile, getListCatalog, getDefaultLists, getRegionalLists, resourcesComponentId, regionalCatalogComponentId } from '../lib/adBlockRustUtils.js'
 import util from '../lib/util.js'
 import path from 'path'
