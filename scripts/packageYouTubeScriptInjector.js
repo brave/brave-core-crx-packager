71b5fe40d91419cc4eed4f3c4a08cda51aa57818
Simone Arpe <simon.arpe@gmail.com>
2025-03-21 12:42:08 +0100

    Add YouTube script injector component data

---
+++ scripts/packageYouTubeScriptInjector.js
@@ -0,0 +1,55 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+// Example usage:
+//  npm run package-youtube-script-injector -- --binary "/Applications/Google\\ Chrome\\ Canary.app/Contents/MacOS/Google\\ Chrome\\ Canary" --key-file path/to/key.pem
+
+import commander from 'commander'
+import fs from 'fs-extra'
+import { mkdirp } from 'mkdirp'
+import path from 'path'
+import util from '../lib/util.js'
+import ntpUtil from '../lib/ntpUtil.js'
+
+const getOriginalManifest = () => {
+  return path.join(path.resolve(), 'component-data', 'youtube-script-injector', 'manifest.json')
+}
+
+const stageFiles = util.stageDir.bind(
+  undefined,
+  path.join(path.resolve(), 'component-data', 'youtube-script-injector'),
+  getOriginalManifest())
+
+const generateCRXFile = (binary, endpoint, region, componentID, privateKeyFile,
+  publisherProofKey, publisherProofKeyAlt) => {
+  const stagingDir = path.join('build', 'youtube-script-injector')
+  const crxFile = path.join(stagingDir, 'youtube-script-injector.crx')
+  mkdirp.sync(stagingDir)
+  util.getNextVersion(endpoint, region, componentID).then((version) => {
+    stageFiles(version, stagingDir)
+    util.generateCRXFile(binary, crxFile, privateKeyFile, publisherProofKey,
+      publisherProofKeyAlt, stagingDir)
+    console.log(`Generated ${crxFile} with version number ${version}`)
+  })
+}
+
+util.installErrorHandlers()
+
+util.addCommonScriptOptions(
+  commander
+    .option('-k, --key-file <file>', 'file containing private key for signing crx file'))
+  .parse(process.argv)
+
+let privateKeyFile = ''
+if (fs.existsSync(commander.keyFile)) {
+  privateKeyFile = commander.keyFile
+} else {
+  throw new Error('Missing or invalid private key')
+}
+
+util.createTableIfNotExists(commander.endpoint, commander.region).then(() => {
+  const [, componentID] = ntpUtil.generatePublicKeyAndID(privateKeyFile)
+  generateCRXFile(commander.binary, commander.endpoint, commander.region,
+    componentID, privateKeyFile, commander.publisherProofKey, commander.publisherProofKeyAlt)
+})
